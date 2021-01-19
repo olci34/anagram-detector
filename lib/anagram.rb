f00f307d7a0ca30ca08be0eb3b8107c27d29eb7f
github username <github email address>
2021-01-19 14:56:49 -0500

    commit tree be958288d630447f5a121a23e8ca65b934021736

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        result_array = []
+        result_array = array.select {|word| @word.split("").sort.join == word.split("").sort.join}
+    end
+
+
+end
+
